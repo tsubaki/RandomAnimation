bdfe5d3ff6ba387067e2cd6f2d98d30fa9777e22
tatsuhiko yamamura <tatsuhiko@unity3d.com>
2017-11-01 11:50:10 +0900

    first commit

---
+++ Assets/RandomBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBehaviour : StateMachineBehaviour
+{
+	[SerializeField]int range;
+
+	private static int hashRandom = Animator.StringToHash ("Random");
+
+	public override void OnStateMachineEnter (Animator animator, int stateMachinePathHash)
+	{
+		animator.SetInteger (hashRandom, UnityEngine.Random.Range (0, range));
+	}
+}
